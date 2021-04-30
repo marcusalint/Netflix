13d7032e9f5f8924587c17fc93eb2d6ddd90de20
Salar <salar.taeb@hotmail.com>
2021-04-30 13:44:44 -0700

    adding styled jumbotron components

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import jumboData from './fixtures/jumbo.json';
+import Jumbotron from './components/Jumbotron';
 
-
-function App() {
+export default function App() {
   return (
-    <p>Hello</p>
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <p>{item.title}</p>
+            <p>{item.subTitle}</p>
+            <p>{item.image}</p>
+            <p>{item.alt}</p>
+          </Jumbotron> 
+        ))}
+      </Jumbotron.Container>
   );
 }
 
-export default App;
+
