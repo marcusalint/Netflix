7a34232ff13ed37cd6cde331b368dc776ac7ef6f
Salar <salar.taeb@hotmail.com>
2021-05-18 23:14:40 -0700

    adding styled components for Pane, Title, SubTitle, Image

--- netflix/src/components/Jumbotron/styles/jumbotron.js
+++ netflix/src/components/Jumbotron/styles/jumbotron.js
@@ -7,12 +7,30 @@ export const Inner = styled.div`
   justify-content: space-between;
   max-width: 1100px;
   margin: auto;
-  width: 100%
+  width: 100%;
 
   @media (max-width: 1000px) {
     flex-direction: column;
   }
 `;
+
+export const Pane = styled.div`
+  width: 50%;
+
+`
+
+export const Title = styled.h1`
+  
+`
+
+export const SubTitle = styled.h2`
+  
+`
+
+export const Image = styled.img`
+  
+  
+`
 
 export const Container = styled.div`
   
