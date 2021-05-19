7a6cb4c4e898e849279686489c5c97aef2076509
Salar <salar.taeb@hotmail.com>
2021-05-19 01:47:05 -0700

    adding accordion elements

---
+++ netflix/src/components/accordion/index.js
@@ -0,0 +1,36 @@
+import React, {useState, useContext, createContext} from 'react';
+
+const ToggleContext = createContext
+
+export default function Accordion({children, ...restProps}) {
+  return(
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+
+Accordion.Title = function AccordionTitle({children, ...restProps}) {
+  return <Title {...restProps}>{children}</Title>
+};
+
+Accordion.Frame = function AccordionFrame({children, ...restProps}) {
+  return <Frame {...restProps}>{children}</Frame>
+};
+
+Accordion.Item = function AccordionItem({children, ...restProps}) {
+  const [toggleShow, setToggleShow] = useState(false)
+  return <Item {...restProps}>{children}</Item>
+};
+
+Accordion.Header = function AccordionHeader({children, ...restProps}) {
+  return <Header {...restProps}>{children}</Header>
+};
+
+Accordion.Header = function AccordionHeader({children, ...restProps}) {
+  return (
+    <Header onClick={() => setToggle()} {...restProps}>
+      {children}
+    </Header>
+  )
+};
