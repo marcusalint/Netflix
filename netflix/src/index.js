960ca93a9b9ce779ce3753fe31867e0b88d3a947
Salar <salar.taeb@hotmail.com>
2021-05-19 00:08:09 -0700

    formatting

--- netflix/src/index.js
+++ netflix/src/index.js
@@ -1,13 +1,12 @@
-import React from "react";
-import { render } from "react-dom";
-import "normalize.css";
-import App from "./app";
-import { GlobalStyles } from "./global-styles";
+import React from 'react';
+import {render} from 'react-dom';
+import 'normalize.css';
+import App from './app';
+import {GlobalStyles} from './global-styles';
 
 render(
-  <>
-    <GlobalStyles />
-    <App />
-  </>,
-  document.getElementById("root")
+<><GlobalStyles /><App /></>,
+document.getElementById('root')
 );
+
+
