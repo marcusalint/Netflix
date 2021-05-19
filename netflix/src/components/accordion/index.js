7721b114aaee36f46d0f3fd78189ebc937849eec
Salar <salar.taeb@hotmail.com>
2021-05-19 03:09:22 -0700

    importing accordion components from styles

--- netflix/src/components/accordion/index.js
+++ netflix/src/components/accordion/index.js
@@ -1,4 +1,5 @@
 import React, {useState, useContext, createContext} from 'react';
+import {Container, Frame, Title, Item, Inner, Header, Body} from './styles/accordion';
 
 const ToggleContext = createContext;
 
@@ -36,3 +37,8 @@ Accordion.Header = function AccordionHeader({children, ...restProps}) {
   )
 };
 
+Accordion.Body = function AccordionBody({children, ...restProps}) {
+  const {toggleShow} = useContext(ToggleContext)
+
+  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+};
