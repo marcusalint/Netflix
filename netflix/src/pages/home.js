31865d8970d3cd8787ded0ee34e09f0e06df058d
Salar <salar.taeb@hotmail.com>
2021-05-20 01:47:22 -0700

    nesting the optform component in the headercontainer

--- netflix/src/pages/home.js
+++ netflix/src/pages/home.js
@@ -13,14 +13,15 @@ export default function Home() {
         <Feature>
           <Feature.Title>Unlimited films, TV Programs and more.</Feature.Title>
           <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
-        </Feature>
-        <OptForm>
+          <OptForm>
           <OptForm.input placeholder="Email Address" />
           <OptForm.Button>Try it Now</OptForm.Button>
           <OptForm.Text>
               Ready to watch? Enter your email to create or restart your membership.
           </OptForm.Text>
         </OptForm>
+        </Feature>
+        
       </HeaderContainer>
       <JumbotronContainer/>
       <FaqsContainer />
