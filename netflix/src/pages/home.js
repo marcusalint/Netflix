4406cf513a8a329ca758a0eb42737c70536a791d
Salar <salar.taeb@hotmail.com>
2021-05-20 00:29:05 -0700

    testing routes

--- netflix/src/pages/home.js
+++ netflix/src/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { FaqsContainer } from "../containers/faqs";
+import { FooterContainer } from "../containers/footer";
+import { JumbotronContainer } from "../containers/jumbotron";
 
 export default function Home() {
   return (
