1ae30d59b98345a77b05c4743799e4ce624ac69b
Salar <salar.taeb@hotmail.com>
2021-05-20 04:53:54 -0700

    adding form input and submit elements

--- netflix/src/pages/signin.js
+++ netflix/src/pages/signin.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Form } from "../components";
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
 
-export default function SignIn() {
+
+export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = password == "" || emailAddress == "";
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+  }
+
+
   return(
     <>
       <HeaderContainer>
-        Hello from the SignIn
+        <Form>
+          <Form.Title>Sign in</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+        </Form>
       </HeaderContainer>
       <FooterContainer />
     </>
-  )
+  );
 }
