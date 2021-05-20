ccaa9994133b753c5a9370611d6edb433b437374
Salar <salar.taeb@hotmail.com>
2021-05-20 02:13:28 -0700

    seeding database

--- netflix/src/lib/firebase.prod.js
+++ netflix/src/lib/firebase.prod.js
@@ -1,12 +1,20 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import {seedDatabase} from '../seed';
 
 //  need to somehow seed the database
 
 // we need a config here
 
-const config = {};
+const config = {    
+apiKey: "AIzaSyCzmUJTLsk5ujSqXh3smwpCzsWSXKH1o6Y",
+authDomain: "netflix-4de70.firebaseapp.com",
+projectId: "netflix-4de70",
+storageBucket: "netflix-4de70.appspot.com",
+messagingSenderId: "259940630909",
+appId: "1:259940630909:web:e633296272310bf2d7cc14"
+};
 
 const firebase = Firebase.initializeApp(config);
 
