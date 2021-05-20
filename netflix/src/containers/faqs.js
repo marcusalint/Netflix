525c413eef897074ccd1ec1327d5147577f6496f
Salar <salar.taeb@hotmail.com>
2021-05-19 23:56:32 -0700

    nesting optform in faqs container

--- netflix/src/containers/faqs.js
+++ netflix/src/containers/faqs.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Accordion } from '../components';
+import OptForm from '../components/opt-form';
 import faqsData from '../fixtures/faqs';
 
 export function FaqsContainer() {
   return (
     <Accordion>
       <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      <Accordion.Frame>
+      <Accordion.Item>
         {faqsData.map((item) => (
           <Accordion.Item key={item.id}>
             <Accordion.Header>{item.header}</Accordion.Header>
             <Accordion.Body>{item.body}</Accordion.Body>
           </Accordion.Item>
         ))}
-      </Accordion.Frame>
+      </Accordion.Item>
+
+      <OptForm>
+        <OptForm.input placeholder="Email Address" />
+        <OptForm.Button>Try it Now</OptForm.Button>
+        <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
