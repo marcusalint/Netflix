cf5bedabcaa364be0798f9a788e451f84af8f354
Salar <salar.taeb@hotmail.com>
2021-05-20 00:16:18 -0700

    adding react-router, removing components to home page

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import * as ROUTES from './constants/routes';
 
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer/>
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}></Route>
+
+    </Router>
   );
 }
