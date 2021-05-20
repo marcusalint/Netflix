92d697018da896a66ce7847c989d273578a2255d
Salar <salar.taeb@hotmail.com>
2021-05-19 23:24:44 -0700

    finalizing faqs container

--- netflix/src/containers/faqs.js
+++ netflix/src/containers/faqs.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Accordion } from '../components';
-import faqsData from '../fixtures/faqs.json';
+import faqsData from '../fixtures/faqs';
 
 export function FaqsContainer() {
   return (
     <Accordion>
       <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      {faqsData.map((item) => {
-        <Accordion.Item key={item.id}>
-          <Accordion.Header>{item.header}</Accordion.Header>
-          <Accordion.Body>{item.body}</Accordion.Body>
-        </Accordion.Item>
-      })};
+      <Accordion.Frame>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion.Frame>
     </Accordion>
-  )
+  );
 }
-
-
-
