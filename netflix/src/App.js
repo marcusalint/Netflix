ce455da77ef956dd1a343be31954a25947ef5fc5
Salar <salar.taeb@hotmail.com>
2021-05-20 23:36:21 -0700

    adding user authentication from react-router

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Home, Browse, Signin, Signup} from './pages';
+import {IsUserRedirect} from './helpers/routes';
 
 import * as ROUTES from './constants/routes';
 
 
 export default function App() {
+  const user={};
+
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path="/signin">
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <Signin />
-      </Route>
+      </IsUserRedirect>
       <Route exact path="/signup">
         <Signup />
+      </Route>
+      <Route exact path="/browse">
+        <Browse />
       </Route>
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
-
     </Router>
   );
 }
