bde0bcba1482b5763d05d9b426967bd97b03f8dc
Salar <salar.taeb@hotmail.com>
2021-05-20 23:56:40 -0700

    finishing user verification

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Home, Browse, Signin, Signup} from './pages';
-import {IsUserRedirect} from './helpers/routes';
+import {IsUserRedirect, ProtectedRoute} from './helpers/routes';
 
 import * as ROUTES from './constants/routes';
 
 
 export default function App() {
-  const user={};
+  const user= {
+    name: "Salar"
+  };
 
   return (
     <Router>
@@ -15,19 +17,30 @@ export default function App() {
         user={user}
         loggedInPath={ROUTES.BROWSE}
         path={ROUTES.SIGN_IN}
-        exact
       >
         <Signin />
       </IsUserRedirect>
-      <Route exact path="/signup">
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+      >
         <Signup />
-      </Route>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+      <ProtectedRoute 
+        user={user}
+        path={ROUTES.BROWSE}
+        >
+          <Browse/>
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   );
 }
