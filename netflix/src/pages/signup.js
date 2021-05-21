2b69d85c12774c71ec225da61db31e797b24cba4
Salar <salar.taeb@hotmail.com>
2021-05-20 22:45:50 -0700

    finishing building components

--- netflix/src/pages/signup.js
+++ netflix/src/pages/signup.js
@@ -15,11 +15,47 @@ export default function SignUp() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const isInvalid = firstName == "" || emailAddress == "" || password == "";
+  const isInvalid = firstName === "" || emailAddress === "" || password === "";
+
+  const handleSignUp = (event) => {
+    event.preventDefault()
+  }
 
   return(
-
-
-    <p>Hello from the signup</p>
+    <>
+      <HeaderContainer>
+      <Form>
+          <Form.Title>Sign in</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit = {handleSignUp} method="POST">
+            <Form.Input
+                placeholder="First name"
+                value={firstName}
+                onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Email Address"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+            <Form.Text>
+              Already a user? <Form.Link to="/signin">Sign in now</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This pase is protected by Google reCAPTCHA to ensure you're not a robot. Learn more.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   )
 }
