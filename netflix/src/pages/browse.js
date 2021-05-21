0eef784a5897ec5204b7dfedb3bf3ff0cd11a7e7
Salar <salar.taeb@hotmail.com>
2021-05-21 00:34:31 -0700

    importing useContent hook

--- netflix/src/pages/browse.js
+++ netflix/src/pages/browse.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import {useContent} from '../hooks';
 
 export default function Browse() {
+  // we need series and films
+  const { series }  = useContent('series');
+  const { films }  = useContent('films');
+  
+
+  // we need slides
+  // pass it to the browser container
+  
+
+
   return(
     <p>Hello from the Browse</p>
   )
