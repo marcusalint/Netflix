fbea1a1ec64bc8587364738e0c91845165a4d4d4
Salar <salar.taeb@hotmail.com>
2021-05-21 00:22:16 -0700

    importing useAuthListener

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Home, Browse, Signin, Signup} from './pages';
 import {IsUserRedirect, ProtectedRoute} from './helpers/routes';
+import {useAuthListener} from './hooks';
 
 import * as ROUTES from './constants/routes';
 
 
 export default function App() {
-  const user= {
-    name: "Salar"
-  };
+  const {user} = useAuthListener();
+  console.log(user)
 
   return (
     <Router>
