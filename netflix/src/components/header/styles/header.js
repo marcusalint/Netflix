806644493120d65e484727d5c1105eb7efca1a40
Salar <salar.taeb@hotmail.com>
2021-05-22 20:42:39 -0700

    initializing card component

--- netflix/src/components/header/styles/header.js
+++ netflix/src/components/header/styles/header.js
@@ -48,13 +48,14 @@ export const PlayButton = styled.button`
   box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
   background-color: #e6e6e6;
   color: #000;
+  font-weight: bold;
   border-width: 0;
   padding: 10px 20px;
   border-radius: 5px;
   max-width: 130px;
   font-weight: bold;
   font-size: 20px;
-  margin-top: 10px;
+  margin-top: 30px;
   cursor: pointer;
   transition: background-color 0.5s ease;
   &:hover {
