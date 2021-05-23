11460344c3429533b91de1446dea5db8796a3469
Salar <salar.taeb@hotmail.com>
2021-05-22 18:10:27 -0700

    adding header to browse page

--- netflix/src/containers/browse.js
+++ netflix/src/containers/browse.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react';
 import {SelectProfileContainer} from './profiles';
 import {FirebaseContext} from '../context/firebase';
-import { Loading } from '../components';
+import { Loading, Header } from '../components';
+
 
 
 export  function BrowseContainer({slides}) {
@@ -21,10 +22,15 @@ export  function BrowseContainer({slides}) {
   }, [profile.displayName])
 
   return profile.displayName ?  (
-    loading ? (
-    <Loading src={user.photoURL} /> ) : null ) 
-    : 
-    (
+    <>
+    {
+    loading ? <Loading src={user.photoURL} /> 
+    :
+    <Loading.ReleaseBody /> 
+    }
+    <Header src="joker1"><p>hello</p></Header> 
+    </>
+    ) : (
     <SelectProfileContainer user={user} setProfile={setProfile}/>
     )
 }
