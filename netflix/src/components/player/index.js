5ecf4478d0da6f923e6326aa4bddf62d4d7a12ad
Salar <salar.taeb@hotmail.com>
2021-05-26 04:06:18 -0700

    recommit

--- netflix/src/components/player/index.js
+++ netflix/src/components/player/index.js
@@ -26,7 +26,7 @@ Player.Video = function PlayerVideo({src, ...restProps }) {
   const {showPlayer, setShowPlayer} = useContext(PlayerContext)
 
   return showPlayer ? ReactDOM.createPortal(
-    <Overlay onClick={() => setShowPlayer(false)}>
+    <Overlay  onClick={() => setShowPlayer(false)} {...restProps}>
       <Inner>
         <video id="netflix-player" controls>
           <source src={src} type="video/mp4" />
