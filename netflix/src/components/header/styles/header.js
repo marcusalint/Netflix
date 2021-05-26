e1885ef991ffe5c1c231493852e3bafd4570d361
Salar <salar.taeb@hotmail.com>
2021-05-26 04:06:10 -0700

    recommit

--- netflix/src/components/header/styles/header.js
+++ netflix/src/components/header/styles/header.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components/macro';
-import { Link as ReactRouterLink } from 'react-router-dom';
+import { Link as ReachRouterLink } from 'react-router-dom';
 
 export const Background = styled.div`
   display: flex;
   flex-direction: column;
   background: linear-gradient(to bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.35)), url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover
     no-repeat;
+
   @media (max-width: 1100px) {
     ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
-}
+  }
 `;
 
 export const Container = styled.div`
@@ -17,9 +18,11 @@ export const Container = styled.div`
   height: 100px;
   justify-content: space-between;
   align-items: center;
+
   a {
     display: flex;
   }
+
   @media (max-width: 1000px) {
     margin: 0 30px;
   }
@@ -31,9 +34,11 @@ export const Link = styled.p`
   margin-right: 30px;
   font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
   cursor: pointer;
+
   &:hover {
     font-weight: bold;
   }
+
   &:last-of-type {
     margin-right: 0;
   }
@@ -42,26 +47,6 @@ export const Link = styled.p`
 export const Group = styled.div`
   display: flex;
   align-items: center;
-`;
-
-export const PlayButton = styled.button`
-  box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
-  background-color: #e6e6e6;
-  color: #000;
-  font-weight: bold;
-  border-width: 0;
-  padding: 10px 20px;
-  border-radius: 5px;
-  max-width: 130px;
-  font-weight: bold;
-  font-size: 20px;
-  margin-top: 30px;
-  cursor: pointer;
-  transition: background-color 0.5s ease;
-  &:hover {
-    background-color: #ff1e1e;
-    color: white;
-  }
 `;
 
 export const SearchInput = styled.input`
@@ -76,6 +61,7 @@ export const SearchInput = styled.input`
   padding: ${({ active }) => (active === true ? '0 10px' : '0')};
   opacity: ${({ active }) => (active === true ? '1' : '0')};
   width: ${({ active }) => (active === true ? '200px' : '0px')};
+
   &:focus {
     background-color: rgba(0, 0, 0, 0.8);
   }
@@ -84,10 +70,12 @@ export const SearchInput = styled.input`
 export const Search = styled.div`
   display: flex;
   align-items: center;
+
   svg {
     color: white;
     cursor: pointer;
   }
+
   @media (max-width: 700px) {
     display: none;
   }
@@ -104,13 +92,14 @@ export const SearchIcon = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+
   img {
     filter: brightness(0) invert(1);
     width: 16px;
   }
 `;
 
-export const ButtonLink = styled(ReactRouterLink)`
+export const ButtonLink = styled(ReachRouterLink)`
   display: block;
   background-color: #e50914;
   width: 84px;
@@ -122,6 +111,7 @@ export const ButtonLink = styled(ReactRouterLink)`
   padding: 8px 17px;
   cursor: pointer;
   text-decoration: none;
+
   &:hover {
     background: #f40612;
   }
@@ -144,24 +134,31 @@ export const Dropdown = styled.div`
   width: 100px;
   top: 32px;
   right: 10px;
+
   ${Group}:last-of-type ${Link} {
     cursor: pointer;
   }
+
   ${Group} {
     margin-bottom: 10px;
+
     &:last-of-type {
       margin-bottom: 0;
     }
+
     ${Link} {
       cursor: pointer;
     }
+
     ${Picture} {
       cursor: default;
     }
   }
+
   button {
     margin-right: 10px;
   }
+
   p {
     font-size: 12px;
     margin-bottom: 0;
@@ -174,9 +171,11 @@ export const Profile = styled.div`
   align-items: center;
   margin-left: 20px;
   position: relative;
+
   button {
     cursor: pointer;
   }
+
   &:hover > ${Dropdown} {
     display: flex;
     flex-direction: column;
@@ -188,6 +187,7 @@ export const Feature = styled(Container)`
   flex-direction: column;
   align-items: normal;
   width: 50%;
+
   @media (max-width: 1100px) {
     display: none;
   }
@@ -213,9 +213,29 @@ export const Logo = styled.img`
   height: 36px;
   width: 134px;
   margin-right: 40px;
+
   @media (min-width: 1449px) {
     height: 45px;
     width: 167px;
   }
 `;
 
+export const PlayButton = styled.button`
+  box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+  background-color: #e6e6e6;
+  color: #000;
+  border-width: 0;
+  padding: 10px 20px;
+  border-radius: 5px;
+  max-width: 130px;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 10px;
+  cursor: pointer;
+  transition: background-color 0.5s ease;
+
+  &:hover {
+    background-color: #ff1e1e;
+    color: white;
+  }
+`;
